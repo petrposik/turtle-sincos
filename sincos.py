3b04002dfa35bd72c7484fd4a319300b4467aa9b
Petr Pošík <petr.posik@gmail.com>
2023-07-23 12:30:53 +0200

    Add red dot tracking the horizotal position of the main dot

--- sincos.py
+++ sincos.py
@@ -5,6 +5,7 @@ ANGULAR_SPEED = 1
 DOT_SIZE = 5
 BLUE = (0, 160/255, 193/255)
 YELLOW = (248/255, 237/255, 49/255)
+RED = (242/255, 114/255, 124/255)
 
 def init_turtle_screen():
     window = turtle.Screen()
@@ -27,9 +28,11 @@ if __name__ == "__main__":
     main_dot = create_dot(BLUE, (0, -RADIUS))
     main_dot.pendown()
     vertical_dot = create_dot(YELLOW, (main_dot.xcor() + 2*RADIUS, main_dot.ycor()))
+    horizontal_dot = create_dot(RED, (main_dot.xcor(), main_dot.ycor() - RADIUS))
     while True:
         main_dot.circle(RADIUS, ANGULAR_SPEED)
         vertical_dot.sety(main_dot.ycor())
+        horizontal_dot.setx(main_dot.xcor())
         window.update()
 
     turtle.done()
