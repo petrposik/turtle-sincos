a368c915377175f9a90383a49e8fa6bd8dec626f
Petr Pošík <petr.posik@gmail.com>
2023-07-23 12:15:43 +0200

    Add a yellow dot moving vertically according to blue dot

--- sincos.py
+++ sincos.py
@@ -15,8 +15,15 @@ main_dot.penup()
 main_dot.setposition(0, -RADIUS)
 main_dot.pendown()
 
+vertical_dot = turtle.Turtle()
+vertical_dot.shape("circle")
+vertical_dot.color(248/255, 237/255, 49/255)
+vertical_dot.penup()
+vertical_dot.setposition(main_dot.xcor() + 2*RADIUS, main_dot.ycor())
+
 while True:
     main_dot.circle(RADIUS, ANGULAR_SPEED)
+    vertical_dot.sety(main_dot.ycor())
     window.update()
 
 turtle.done()
